8a37834d172b7bc131c7cff5ef9333099e82c8fc
LloydTao <LL23@live.co.uk>
2020-04-23 12:35:29 +0100

    Create views for landing, team and members

--- src/home/views.py
+++ src/home/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from django.contrib.auth.models import User
+from .models import Project
+
+
+class LandingView():
+    template_name = 'blog/landing.html'
+
+
+class TeamView(ListView):
+    model = User
+    template_name = 'blog/team.html'
+    context_object_name = 'members'
+    ordering = ['date']
+
+
+class TeamMemberView(ListView):
+    model = Project
+    template_name = 'blog/team-member.html'
+    context_object_name = 'projects'
+    ordering = ['-date']
+    paginate_by = 10
+    
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Project.objects.filter(author=user).order_by('-date')
