93eaa484772d54e6799ec552a552ddc60c8ef5b9
LloydTao <LL23@live.co.uk>
2020-04-23 20:12:19 +0100

    Fix paths of 'home' app templates

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, TemplateView
 
@@ -7,20 +5,20 @@ from django.contrib.auth.models import User
 from .models import Project
 
 
-class LandingView():
-    template_name = 'blog/landing.html'
+class LandingView(TemplateView):
+    template_name = 'home/landing.html'
 
 
 class TeamView(ListView):
     model = User
-    template_name = 'blog/team.html'
+    template_name = 'home/home-team.html'
     context_object_name = 'members'
     ordering = ['date']
 
 
 class TeamMemberView(ListView):
     model = Project
-    template_name = 'blog/team-member.html'
+    template_name = 'home/home-team-member.html'
     context_object_name = 'projects'
     ordering = ['-date']
     paginate_by = 10
