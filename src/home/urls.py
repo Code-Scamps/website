7287c221a72eaf11c6079c38bf28a37a8f27f50d
LloydTao <LL23@live.co.uk>
2020-04-23 11:29:22 +0100

    Add routing to team and member pages

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import LandingView
+from .views import LandingView, TeamView, TeamMemberView
 from . import views
 
 urlpatterns = [
     path('', LandingView.as_view(), name='home-landing'),
+    path('team', TeamView.as_view(), name='home-team'),
+    path('team/<str:username>', TeamMemberView.as_view(), name='home-team-member'),
 ]
