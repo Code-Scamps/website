2f5c548cbf9d7ae666e97b1452b7f6618d591ba8
LloydTao <LL23@live.co.uk>
2020-04-23 12:37:44 +0100

    Add routes to login and logout

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import LandingView, TeamView, TeamMemberView
 from . import views
 
@@ -6,4 +7,6 @@ urlpatterns = [
     path('', LandingView.as_view(), name='home-landing'),
     path('team', TeamView.as_view(), name='home-team'),
     path('team/<str:username>', TeamMemberView.as_view(), name='home-team-member'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
