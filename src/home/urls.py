400ee7cc0805a30cb851b6195762eea87f65369e
LloydTao <LL23@live.co.uk>
2020-04-23 12:37:58 +0100

    Add route to update profile

--- src/home/urls.py
+++ src/home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import LandingView, TeamView, TeamMemberView
 from . import views
+from users import views as user_views
 
 urlpatterns = [
     path('', LandingView.as_view(), name='home-landing'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('team/<str:username>', TeamMemberView.as_view(), name='home-team-member'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
