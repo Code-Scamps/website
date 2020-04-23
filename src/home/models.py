6bd8cc50be7656da699c5d37222f04da218945a2
LloydTao <LL23@live.co.uk>
2020-04-23 19:30:00 +0100

    Add Project model to 'home' app

--- src/home/models.py
+++ src/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=128)
+    date = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(default=timezone.now)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
