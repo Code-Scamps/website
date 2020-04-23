76d311fc77294b47f2b955a6160233e913ea7e7d
LloydTao <LL23@live.co.uk>
2020-04-23 20:11:13 +0100

    Import get_object_or_404 to home.views

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
 
 from django.contrib.auth.models import User
 from .models import Project
